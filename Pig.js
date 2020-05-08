59c4c84d1e5ac26a0b7a59ab7ae7f3ff47c0d8af
Maanav2 <maanavsharat792@gmail.com>
2020-05-08 20:52:40 +0530

    commit this

--- Pig.js
+++ Pig.js
@@ -2,6 +2,22 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibility=255;
   }
-
+display(){
+  //super.display();
+  
+  console.log(this.body.speed);
+  if(this.body.speed>6){
+    World.remove(world,this.body);
+    push();
+    this.visibility=this.visibility-10;
+    tint(255,this.visibility)
+    image(this.image,this.body.position.x,this.body.position.y,this.width,this.height)
+    pop();
+  }
+  else{
+    super.display();
+  }
+}
 };
